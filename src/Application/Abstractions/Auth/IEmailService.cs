4ef6b39f8b00e7716d37c0dc5f3dca420270cdea
mariyamikhailovna <itszemoff@gmail.com>
2026-09-16 11:10:47 +0800

    removed an imcomplete line that caused a crash

--- src/Application/Abstractions/Auth/IEmailService.cs
+++ src/Application/Abstractions/Auth/IEmailService.cs
@@ -4,6 +4,5 @@ namespace Application.Abstractions.Auth
     public interface IEmailService
     {
         Task VerifyEmailAsync(Guid UserId, string email, string VerificationUrl);
-        Task 
     }
 }
